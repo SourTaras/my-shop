cb8fa6660bd9a92364388ef20a42d168586246aa
SourTaras <velgantaras03@gmail.com>
2025-08-11 17:25:55 +0300

    Footer

---
+++ my-shop/src/components/FooterComponent.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+<h2>@2025 Всі права захищено</h2>
+<ul>
+<li>{{ email }} </li>
+<li>{{ phone }} </li>
+</ul>
+<ul>
+<li><a>{{ about }} </a></li>
+<li><a> {{ delivery }} </a></li>
+</ul>
+</div>
+</template>
+<script>
+export default {
+    props: {
+        email: String,
+        phone: Number,
+        about: String,
+        delivery: String
+    }
+}
+</script>
